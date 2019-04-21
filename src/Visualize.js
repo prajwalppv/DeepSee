4d4c980d79ffbf7d5841b7ff77ffb02e35d94220
Prajwal Prakash Vasisht <prajwalppv@gmail.com>
2019-04-21 18:44:13 -0400

    fixed sem dict bug

--- src/Visualize.js
+++ src/Visualize.js
@@ -7,14 +7,14 @@ import ImagePertubation from "./ImagePertubation"
 import GuidedBackProp from "./GuidedBackProp"
 import Saliency from "./Saliency"
 import App from "./App";
-import SemanticDictionary from "./SemanticDictionary";
+import SemDictUpload from "./SemanticDictionaryUpload";
 // import {BrowserRouter as Router,Route}  from 'react-router-dom';
 
 class Visualize extends Component{
     state={
         selectedIndex:0,
         tabs: ['Image Perturbation','Saliency Maps','Guided BackProp','Channel Activations','Semantic dictionaries'],
-        components: [<ImagePertubation/>,<Saliency/>,<GuidedBackProp/>,<App/>,<SemanticDictionary/>],
+        components: [<ImagePertubation/>,<Saliency/>,<GuidedBackProp/>,<App/>,<SemDictUpload/>],
         icons:['home','series-configuration','cloud-upload','help']
     }
 
@@ -23,7 +23,7 @@ class Visualize extends Component{
            <div>
                <Pane display="flex" height={240}>
                     <Tablist marginBottom={16} flexBasis={240} marginRight={10}>
-                        <Pane borderTop borderRight borderLeft borderBottom 
+                        <Pane borderTop borderRight borderLeft borderBottom
                         borer='muted'
                         background='tint1'
                         elevation={1}
@@ -53,7 +53,7 @@ class Visualize extends Component{
                             aria-labelledby={tab}
                             aria-hidden={index !== this.state.selectedIndex}
                             display={index === this.state.selectedIndex ? 'block' : 'none'}
-                        > 
+                        >
                             {this.state.components[index]}
                         </Pane>
                             ))}
