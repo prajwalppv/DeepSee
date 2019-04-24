2bfd1a87b8ad8fdcddb866971cac831986d74ee7
Manisha Chaurasia <mchauras@andrew.cmu.edu>
2019-04-23 22:07:30 -0400

    New viz added

--- src/server_lucid/server_lucid.py
+++ src/server_lucid/server_lucid.py
@@ -3,8 +3,9 @@ from flask_cors import CORS, cross_origin
 import base64
 from flask import request
 from semanticDict import googlenet_semantic_dict
-
-
+import os
+from svelte_python.neuron_groups import callNeuronGroups
+from svelte_python.spatial_attr import callSpatialAttr
 app = flask.Flask(__name__)
 
 
@@ -31,6 +32,27 @@ def semanticDictionary():
 
     results['success'] = True
 
+    return flask.jsonify(results)
+
+@app.route("/neuronGroups", methods=['POST'])
+@cross_origin()
+def neuronGroups():
+    layer = flask.request.form['layer']
+    group = flask.request.form['group']
+    results = {'success':False}
+    callNeuronGroups("currentImage.png", str(layer), int(group))
+    results['success'] = True
+    return flask.jsonify(results)
+
+@app.route("/spatialAttribution", methods=['POST'])
+@cross_origin()
+def spatialAttribution():
+    layer1 = flask.request.form['layer1']
+    layer2 = flask.request.form['layer2']
+    results = {'success':False}
+    print(layer1, type(layer1), str(layer2), type(layer2))
+    callSpatialAttr("currentImage.png", str(layer1), str(layer2))
+    results['success'] = True
     return flask.jsonify(results)
 
 if __name__ == "__main__":
