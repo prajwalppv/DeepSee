a4e4d3b0e87cf03c6752804d06c2ed603e76692f
Manisha Chaurasia <mchauras@andrew.cmu.edu>
2019-04-24 12:38:44 -0400

    Iframe part modified

--- src/NeuronGroups.js
+++ src/NeuronGroups.js
@@ -6,25 +6,20 @@ import Iframe from 'react-iframe'
 
 
 class NeuronGroups extends Component{
+shouldComponentUpdate() {
+ return false;
+}
 render(){
     return(
-    <Iframe url="https://nervous-yonath-588b40.netlify.com/"
-            width="1200px"
-            height="1450px"
+
+    <Iframe url="http://localhost:3000/neuron_group.html"
+            width="500px"
+            height="350px"
             id="myId"
             className="myClassname"
             display="initial"
             position="relative"/>
             );
-
-//    <Iframe url="http://localhost:3000/neuron_group.html"
-//            width="500px"
-//            height="350px"
-//            id="myId"
-//            className="myClassname"
-//            display="initial"
-//            position="relative"/>
-//            );
     }
 }
 export default NeuronGroups;
