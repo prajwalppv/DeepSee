a46a82c55e4a333fd2d24d83fb0796273ac3a028
Manisha Chaurasia <mchauras@andrew.cmu.edu>
2019-04-24 21:20:11 -0400

    Urls vars added by importing DNS file

--- src/NeuronGroups.js
+++ src/NeuronGroups.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import LoadingOverlay from 'react-loading-overlay';
 import { Button, TextInputField } from 'evergreen-ui'
 import Iframe from 'react-iframe'
+import * as values from "./dns"
 
-
+const dns = values.AWSDNS + ":3000/neuron_group.html";
+console.log(dns)
 
 class NeuronGroups extends Component{
+
 shouldComponentUpdate() {
  return false;
 }
 render(){
     return(
 
-    <Iframe url="http://localhost:3000/neuron_group.html"
+    <Iframe url={dns}
             width="500px"
             height="350px"
             id="myId"
